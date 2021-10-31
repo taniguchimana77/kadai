dc907a40ee87ac1d7708b5690791c7b1b18b2da9
taniguchimana77 <tani.jugyou173@gmail.com>
2021-10-31 15:12:35 +0000

    課題1を提出

---
+++ kadai02/index.js
@@ -0,0 +1,15 @@
+const firstName = "hanako"
+const familyName = "yamada"
+console.log(firstName + familyName)
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const dog = true
+const cat = false
+console.log(dog||cat)//犬または猫を飼っているか
+console.log(dog&&cat)//犬と猫両方飼っている
+console.log(!dog&&!cat)//犬も猫も飼ってない
+console.log(!(dog||cat))
+
+console.log(cat&&!dog)//猫飼ってる犬が飼ってない
+console.log(dog&&!cat)//犬を飼っているが猫は飼ってない
